7178c38591638f1738d6c522eb840558f4d4f35b
Bruno Silva (silv4bprof) <bruno.borges@escolar.ifrn.edu.br>
2024-03-18 21:13:38 -0300

    Adicionando o pacote difflib para comparação de textos/códigos

--- verificador.py
+++ verificador.py
@@ -1,7 +1,6 @@
-import json
-import os
 import platform
 import tkinter as tk
+import difflib as df
 
 from customtkinter import *
 from tkinter.messagebox import showerror
@@ -12,31 +11,22 @@ SISTEMA = platform.system().lower()
 TEMA = "clam"
 PADDING = 5
 LARGURA_WIDGET_QUESTAO = 694
-DIMENSOES_JANELA = "1024x600"
+DIMENSOES_JANELA = "1024x700"
+FONTE_NATIVA = "Courier"
 
-set_default_color_theme("green")
+set_default_color_theme("blue")
 
 
 class Verificador:
-    """Janela principal do corretor."""
+    """Janela principal do verificador."""
 
     def __init__(self, caminho_config: str):
         """Construtor.
-        `caminho_config` é o caminho para o arquivo json de configuração da correção."""
+        `caminho_config` é o caminho para o arquivo json de configuração da verificação.
+        """
         super().__init__()
         janela = CTk()
         self.janela = janela
-
-        # Lê o arquivo de configuração para criar a Atividade
-        if not os.path.isfile(caminho_config):
-            janela.title(f"Verificador de Código")
-            janela.geometry(DIMENSOES_JANELA)
-            showerror(
-                "Erro",
-                "Arquivo de configuração" + f' "{caminho_config}" não encontrado.',
-            )
-            exit()
-        config = json.load(open(caminho_config, encoding="utf-8"))
 
         # Configura a janela
         janela.title(f"Verificador de Código")
@@ -45,33 +35,17 @@ class Verificador:
         # Montagem da interface
         # O frame principal contém todos os elementos da tela
         # Isso facilita o redimensionamento da janela sem alterar seu conteúdo
-        frame_principal = CTkFrame(janela)
-        self.frame_principal = frame_principal
+        frame_principal = CTkFrame(janela, fg_color="#3d3d3d")
         frame_principal.pack(expand=True, fill=tk.BOTH)
+        self.frame_principal = frame_principal
+
         self._montar_frame_topo()
-
-        # Adicionando campos de texto
-        self.texto_esquerda = CTkTextbox(
-            frame_principal,
-            width=40,
-            height=10,
-        )
-        self.texto_esquerda.pack(
-            side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10
-        )
-
-        self.texto_direita = CTkTextbox(
-            frame_principal,
-            width=40,
-            height=10,
-        )
-        self.texto_direita.pack(
-            side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10
-        )
+        self._montar_frame_resultado()
+        self._montar_frame_textos()
 
     def _montar_frame_topo(self):
         """Monta o frame do topo da tela."""
-        frame_topo = CTkFrame(self.frame_principal, fg_color="#2b2b2b")
+        frame_topo = CTkFrame(self.frame_principal, fg_color="#3d3d3d")
         frame_topo.pack(fill=BOTH)
         self.botao_verificar_todos = CTkButton(
             frame_topo,
@@ -80,25 +54,46 @@ class Verificador:
         )
         self.botao_verificar_todos.pack(padx=PADDING * 4, pady=(PADDING * 4, 0))
 
+    def _montar_frame_textos(self):
+        """Monta o frame com os dois campos de texto para validação"""
+        frame_campos_de_texto = CTkFrame(self.frame_principal, fg_color="#3d3d3d")
+        frame_campos_de_texto.pack(expand=True, fill=BOTH)
+
+        # Adicionando campos de texto
+        self.texto_esquerda = CTkTextbox(
+            frame_campos_de_texto, width=40, height=10, font=(FONTE_NATIVA, 16)
+        )
+        self.texto_esquerda.pack(
+            side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10
+        )
+
+        self.texto_direita = CTkTextbox(
+            frame_campos_de_texto, width=40, height=10, font=(FONTE_NATIVA, 16)
+        )
+        self.texto_direita.pack(
+            side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=10, pady=10
+        )
+
+    def _montar_frame_resultado(self):
+        """Monta o frame do topo da tela."""
+        frame_resultado = CTkFrame(self.frame_principal, fg_color="#3d3d3d")
+        frame_resultado.pack(fill=BOTH)
+        self.label_resultado = CTkLabel(
+            frame_resultado, text="Ratio: 0", font=("Arial", 20)
+        )
+        self.label_resultado.pack(fill=BOTH, padx=10, pady=10)
+
     def _verificar_todos(self):
-        """Testa todas as questões."""
+        """Validando os dois textos"""
         print("Verificando diferenças ...")
-
-    def atualizar(self):
-        """Atualiza este widget."""
-        contador_corretas = 0
-        nota = 0
-        for qw in self.widgets_questoes:
-            if qw.correta:
-                contador_corretas += 1
-                nota += qw.questao.pontos
-        total = len(self.widgets_questoes)
-        texto_resultado = f"Corretas: {contador_corretas} de {total}" + f" ({nota} pts)"
-        self.label_corretas.configure(text=texto_resultado)
-
-        # Redesenha a interface
-        self.janela.update()
-        self.janela.update_idletasks()
+        texto_esquerda = self.texto_esquerda.get(1.0, END)
+        texto_direita = self.texto_direita.get(1.0, END)
+        seq = df.SequenceMatcher(None, texto_esquerda, texto_direita)
+        seq = seq.ratio() * 100
+        resultado = f"Ratio: {seq:.2f}%"
+        print(resultado)
+        # self.label_resultado["text"] = resultado
+        self.label_resultado.configure(text=resultado)
 
 
 if __name__ == "__main__":
